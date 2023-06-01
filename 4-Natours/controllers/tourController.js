69c67f26d7106b7b1be7b0934b29b80ea97c5da0
sass_test <viker_i@163.com>
2023-06-02 01:37:23 +0800

    Initial commit

--- 4-Natours/controllers/tourController.js
+++ 4-Natours/controllers/tourController.js
@@ -1,73 +1,152 @@
-const fs = require('fs');
+const fs = require("fs");
+// const { json } = require("express");
+const Tour = require("./../models/tourModel");
+
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 exports.checkID = (req, res, next) => {
-    console.log('Tour id is: ${val}');
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail', message: 'Invalid ID'
-        });
-    }
-    next();
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail", message: "Invalid ID"
+    });
+  }
+  next();
 };
 
 exports.checkBody = (req, res, next) => {
-    // console.log(req.body);
-    if (!req.body.name || !req.body.price) {
-        //status code 400 : server can't understand
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        })
+  if (!req.body.name || !req.body.price) {
+    //status code 400 : server can't understand
+    return res.status(400).json({
+      status: "fail", message: "Missing name or price"
+    });
+  }
+  next();
+};
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // BUILD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach(el => {
+      delete queryObj[el];
+    });
+
+    // 1B) Advanced Filtering  正则表达式替换
+    let queryStr = JSON.stringify(queryObj); //转化成字符串 gt gte lt lte
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" "); // 第二个标准 在url中用,分割 传入 mongoose用空格分割
+      query = query.sort(req.query.sort);
+    } else {
+      query = query.sort("-createdAt");
+      //默认按照从新到旧创建日期
     }
-    next();
-};
 
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
+    // 3) Field limiting
+    if (req.query.fields) {
+      //带 - 减号是排除
+      const fields = req.query.fields.split(",").join("");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    //分页
+    //page=2&limit=10 , 1-10 page1 , 11-20 page2
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments(); //文档数量
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+    // EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
-        status: 'success', requestTimeAt: req.requestTime, result: tours.length, data: {
-            tours
+      status: "success", result: tours.length, data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail", message: err
+    });
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({ _id : req.params.id })
+    res.status(200)
+      .json({
+        status: "success", data: {
+          tour
         }
+      });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail", message: err
     });
+  }
 };
 
-exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res
+      .status(201)
+      .json({
+        status: "success", data: {
+          tour: newTour
+        }
+      });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
+};
 
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, runValidators: true
+    });
     res.status(200)
-        .json({
-            status: 'success', data: {
-                tour
-            }
-        });
-};
-
-exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), () => {
-        res.status(201).json({
-            status: 'success', data: {
-                tour: newTour
-            }
-        });
+      .json({
+        status: "success", data: {
+          tour
+        }
+      });
+  } catch (err) {
+    res.status(404).json({
+      status: "error", message: err
     });
+  }
 };
 
-exports.updateTour = (req, res) => {
-    res.status(200)
-        .json({
-            status: 'success', data: '<Update tour here...>'
-        });
-};
-
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'success', data: null
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndRemove(req.params.id);
+    res.status(200).json({
+      status: "success", data: null
     });
+  } catch (err) {
+    res.status(404).json({
+      status: "error", message: err
+    });
+  }
 };
--- 4-Natours/server.js
+++ 4-Natours/server.js
@@ -1,7 +1,35 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true
+    //配置项
+  })
+  .then(() => console.log("DB connection successfully!"));
+
+
+//
+// const testTour = new Tour({
+//   name: "The Park Camper",
+//   price: 997
+// });
+
+// const testTour = new Tour({
+//   name:'The Forest Hiker',
+//   rating: 4.7,
+//   price:497
+// });
+
+
+// testTour.save().
+// then(content => console.log(content)).
+// catch( err => console.log("ERROR:",err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
